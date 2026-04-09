2266c4788a4cf416e48958437ea9b830097b7c51
emmer005 <owusuemmanuel005@gmail.com>
2026-04-09 11:33:34 +0000

    Fix Logo and Form Submission Error

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
   <div className="absolute inset-0 flex items-center justify-center">
   <Image
     className="w-full h-full max-w-5xl  object-cover"
-    src="/image.png" width={500} height={500} alt="Coming Soon"/>
+    src="https://ebzpkkgbkxaixjjjjlkl.supabase.co/storage/v1/object/public/FASHNECT/image.png" width={500} height={500} alt="Coming Soon"/>
  </div>
  <main className="relative z-20  backdrop-blur-sm backdrop-brightness-50 flex flex-col items-center r w-full h-full">
           {/* Main Content */}
       <div className="relative z-20  flex-1 gap-4 flex items-center flex-col  px-4 py-8">
         {/* Waitlist Badge */}
         <div>
-          <Image src="https://swqsltuanmnbnmbyqgaq.supabase.co/storage/v1/object/public/fashnect//Fashnect.PNG" className="w-16 h-16 object-cover rounded-xl" width={100} height={100} alt="Fashnect"/>
+          <Image src="https://ebzpkkgbkxaixjjjjlkl.supabase.co/storage/v1/object/public/FASHNECT/Fashnect.PNG" className="w-16 h-16 object-cover rounded-xl" width={100} height={100} alt="Fashnect"/>
         </div>
 
         {/* Main Headline */}
